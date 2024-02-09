41c8c536eac1ecc76c08ee499f7efdcc117e0c53
Asadbek <ravshanbekovasadbek0429@gmail.com>
2024-02-09 20:52:07 +0500

    removed comments

--- blog/views.py
+++ blog/views.py
@@ -20,11 +20,7 @@ def blog_view(request):
     else:
         posts = Post.objects.filter(is_published=True).order_by('-created_at')
 
-    paginator = Paginator(posts, 6) \
- \
-    #
-
-    #
+    paginator = Paginator(posts, 6)
     d = {
         'posts': paginator.page(page),
         'cat_id': cat
